61b0a8a4b0d61058a26ac3eb2b4a6df743955247
Diviner-geeker <577994345@qq.com>
2024-03-07 23:07:10 +0800

    little change

---
+++ build/CMakeFiles/openvino_kpt_class.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nana/Desktop/YOLO-of-RoboMaster-Keypoints-Detection-2023/C++_end2end_openvino_detect/demo.cpp" "CMakeFiles/openvino_kpt_class.dir/demo.cpp.o" "gcc" "CMakeFiles/openvino_kpt_class.dir/demo.cpp.o.d"
+  "/home/nana/Desktop/YOLO-of-RoboMaster-Keypoints-Detection-2023/C++_end2end_openvino_detect/openvino_detect.cpp" "CMakeFiles/openvino_kpt_class.dir/openvino_detect.cpp.o" "gcc" "CMakeFiles/openvino_kpt_class.dir/openvino_detect.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
